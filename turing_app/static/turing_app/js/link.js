2e8d06ca511198a01cc01b3a7759a3a38745747f
lutfiHidayanto <hajidlutfi@gmail.com>
2024-06-22 23:20:31 +0700

    bruh

--- turing_app/static/turing_app/js/link.js
+++ turing_app/static/turing_app/js/link.js
@@ -3,13 +3,18 @@ finalOutput = document.getElementById('tapeOutput2');
 
 inputForm = document.getElementById('final_output');
 
-
 console.log(finalOutput);
 nextButton = document.getElementById('next');
+submitForm = document.getElementById('next-form'); // Get the form element instead of the button
+
 
 nextButton.onclick = () => {
     if (finalOutput.innerHTML == '') {
         finalOutput = document.getElementById('tapeOutput1');
     }
     inputForm.value = finalOutput.innerHTML;
-}
+
+    submitForm.submit();
+};
+
+
